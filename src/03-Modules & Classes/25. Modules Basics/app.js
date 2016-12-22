13041c441da6bfd5b215f99b6aa45722768f9cbe
dongsj <232171773@qq.com>
2016-12-22 13:36:07 +0800

    25. Modules Basics finished

--- src/03-Modules & Classes/25. Modules Basics/app.js
+++ src/03-Modules & Classes/25. Modules Basics/app.js
@@ -2,5 +2,7 @@
  * Created by dongsj on 2016/12/22.
  */
 
-import {keyValue} from './script.js';
+import {keyValue,test} from './script.js';
+console.log(keyValue);
+test();
 console.log(keyValue);
--- src/03-Modules & Classes/25. Modules Basics/script.js
+++ src/03-Modules & Classes/25. Modules Basics/script.js
@@ -1,4 +1,9 @@
 /**
  * Created by dongsj on 2016/12/22.
  */
-export let keyValue=1000;
+let keyValue=1000;
+function test(){
+    keyValue=2000;
+    console.log('test');
+}
+export {keyValue,test};
