7ff7ec2099646eb19f3b6beb405ec1314645a22f
dongsj <232171773@qq.com>
2016-12-23 11:33:50 +0800

    32. Static Methods

---
+++ src/03-Modules & Classes/32. Static Methods .js
@@ -0,0 +1,10 @@
+/**
+ * Created by dongsj on 2016/12/23.
+ */
+class Helper{
+   static logTwice(message){
+        console.log(message);
+        console.log(message);
+    }
+}
+Helper.logTwice('static methods');
