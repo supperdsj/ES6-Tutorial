d2b75af61ea6d8436c0545eba2485790a2a2902d
dongsj <232171773@qq.com>
2016-12-24 19:04:26 +0800

    50. Generators in Action

--- src/05-Iterators & Generators/50. Generators in Action.js
+++ src/05-Iterators & Generators/50. Generators in Action.js
@@ -13,3 +13,14 @@ function *gen(){
 for(let ele of obj){
     console.log(ele);
 }
+
+function *gen2(end){
+    for(let i=0;i<end;i++){
+        yield i;
+    }
+}
+let it=gen2(2);
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
