948172d472bf695fdf2c879decbd1bedf49eef15
dongsj <232171773@qq.com>
2016-12-24 15:25:20 +0800

    42. Advantages of (unique) IDs or Symbols

---
+++ src/04-Symbols/42. Advantages of (unique) IDs or Symbols.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dongsj on 2016/12/24.
+ */
+let symbol1=Symbol.for('age');
+let symbol2=Symbol.for('age');
+console.log(symbol1==symbol2);
+
+let person={
+    name:'dongsj',
+    age:30
+};
+function makeAge(person){
+    let ageSymbol=Symbol.for('age');
+    person[ageSymbol]=27;
+}
+makeAge(person);
+console.log(person['age']);
+console.log(person[symbol1]);
