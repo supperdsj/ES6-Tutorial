00e902be756a736a0776fb081e33ccec9cc5027b
dongsj <232171773@qq.com>
2016-12-26 21:26:02 +0800

    78. Sets - Looping through Sets

--- src/08-Maps & Sets/78. Sets - Looping through Sets.js
+++ src/08-Maps & Sets/78. Sets - Looping through Sets.js
@@ -14,3 +14,16 @@ for (let ele of set.keys()) {
 for (let ele of set.values()) {
     console.log(ele);
 }
+
+let obj={name:1};
+let objSet = new Set([{name:1},{name:1},obj,obj]);
+
+for (let ele of objSet.entries()) {
+    console.log(ele);
+}
+for (let ele of objSet.keys()) {
+    console.log(ele);
+}
+for (let ele of objSet.values()) {
+    console.log(ele);
+}
