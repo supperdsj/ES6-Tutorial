849b7b57a56c05f5bd948e55ea8ef519aa16f343
dongsj <232171773@qq.com>
2016-12-26 17:08:23 +0800

    64. Strings

---
+++ src/07-Extensions of Built-in Objects/64. Strings.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dongsj on 2016/12/26.
+ */
+
+let name='dongsjheihei';
+
+console.log(name.startsWith('dongsj'));
+console.log(name.startsWith('dongsj1111'));
+console.log(name.endsWith('hei'));
+console.log(name.endsWith('HEI'));
+console.log(name.includes('sj'));
+console.log(name.includes('sssj'));
