266c7a2f9717ef0fe03c59aa6e0cef7209d66d8c
dongsj <232171773@qq.com>
2016-12-26 16:46:06 +0800

    63. The Math Object

---
+++ src/07-Extensions of Built-in Objects/63. The Math Object.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dongsj on 2016/12/26.
+ */
+
+console.log(Math.sign(-10));
+console.log(Math.sign(10));
+console.log(Math.sign(0));
+console.log(Math.sign('string'));
+
+console.log(Math.trunc(0.78));
+console.log(Math.trunc(3.78));
+console.log(Math.trunc(-0.78));
+console.log(Math.trunc(-3.78));
+
+console.log(Math.floor(0.78));
+console.log(Math.floor(3.78));
+console.log(Math.floor(-0.78));
+console.log(Math.floor(-3.78));
