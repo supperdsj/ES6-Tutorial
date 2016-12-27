deb959c3521b3730fac555f3ad61d681538d378c
dongsj <232171773@qq.com>
2016-12-27 16:35:46 +0800

    87. Accessing Properties with Reflect

---
+++ src/09-The Reflect API/87. Accessing Properties with Reflect.js
@@ -0,0 +1,31 @@
+/**
+ * Created by dongsj on 2016/12/27.
+ */
+
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this.age = age;
+    }
+
+    get name() {
+        console.log('getter');
+        return this._name;
+    }
+
+    set name(value) {
+        console.log('setter');
+        this._name = value;
+    }
+}
+let mum = {
+    _name: 'Mum'
+};
+let person = new Person('dongsj', 27);
+
+console.log(Reflect.get(person, 'name'));
+console.log(Reflect.get(person, 'name', mum));
+console.log(Reflect.set(person, 'name', 'dsj'));
+console.log(Reflect.set(person, 'name', 'm1', mum));
+console.log(person.name);
+console.log(mum._name);
